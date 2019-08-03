d256a70579b0de32141087fbe994163b1b85a194
Jenarthanan14 <36252800+Jenarthanan14@users.noreply.github.com>
2019-08-03 15:03:46 +0530

    Create 3a.py

---
+++ 3a.py
@@ -0,0 +1,7 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
