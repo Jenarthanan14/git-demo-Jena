844e4b23df31c23af90ec346229c599a7fa1ae66
Jenarthanan14 <36252800+Jenarthanan14@users.noreply.github.com>
2019-08-03 15:55:31 +0530

    Update 3a.py

--- 3a.py
+++ 3a.py
@@ -5,3 +5,38 @@ forest_model = RandomForestRegressor(random_state=1)
 forest_model.fit(train_X, train_y)
 melb_preds = forest_model.predict(val_X)
 print(mean_absolute_error(val_y, melb_preds))
+#################3333
+
+from sklearn.model_selection import GridSearchCV
+# Create the parameter grid based on the results of random search 
+param_grid = {
+    'bootstrap': [True],
+    'max_depth': [80, 90, 100, 110],
+    'max_features': [2, 3],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [8, 10, 12],
+    'n_estimators': [100, 200, 300, 1000]
+}
+# Create a based model
+rf = RandomForestRegressor()
+# Instantiate the grid search model
+grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, 
+                          cv = 3, n_jobs = -1, verbose = 2)
+
+##################
+# Fit the grid search to the data
+grid_search.fit(train_features, train_labels)
+grid_search.best_params_
+{'bootstrap': True,
+ 'max_depth': 80,
+ 'max_features': 3,
+ 'min_samples_leaf': 5,
+ 'min_samples_split': 12,
+ 'n_estimators': 100}
+best_grid = grid_search.best_estimator_
+grid_accuracy = evaluate(best_grid, test_features, test_labels)
+Model Performance
+Average Error: 3.6561 degrees.
+Accuracy = 93.83%.
+print('Improvement of {:0.2f}%.'.format( 100 * (grid_accuracy - base_accuracy) / base_accuracy))
+Improvement of 0.50%.
